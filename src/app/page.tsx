003eb3d9ec4c090503f28853c559c149ef30c0f9
coltran3 <lcssantana03@gmail.com>
2024-11-16 18:40:43 -0300

    feat: add login form component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,25 @@
 import { MarvelIcon } from "@/components/icons/marvel";
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
+import { LoginForm } from "./components/LoginForm";
 
 export default function Home() {
   return (
-    <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link
-        href="/examples/authentication"
-        className={cn(
-          buttonVariants({ variant: "ghost" }),
-          "absolute right-4 top-4 md:right-8 md:top-8"
-        )}
-      >
-        Login
-      </Link>
+    <div className="container relative  h-screen flex-col items-center justify-center flex md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
           <MarvelIcon />
         </div>
       </div>
-      <div className="lg:p-8">
+      <div className="lg:p-8 my-auto">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Entre para continuar
+            </h1>
             <p className="text-sm text-muted-foreground">
-              Insira seu email para entrar
+              Informe um email e uma senha aleatória
             </p>
+            <LoginForm />
           </div>
         </div>
       </div>
