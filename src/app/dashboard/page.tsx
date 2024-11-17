4478ec16755fce681c1678f873bf12b5a061d6e3
coltran3 <lcssantana03@gmail.com>
2024-11-17 20:02:55 -0300

    chore: add card component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,29 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ApplicationCommand } from "./components/command";
+import { useCharacters } from "@/hooks/queries/useCharacters";
 
 export default function Dashboard() {
   const [query, setQuery] = useState("");
 
+  const { data: charactersResponse } = useCharacters({
+    limit: 10,
+    offset: 0,
+    nameStartsWith: query,
+  });
+
+  useEffect(() => {
+    console.log(charactersResponse);
+  });
+
   return (
     <div className="flex flex-col w-full p-3 ">
       <ApplicationCommand value={query} onValueChange={setQuery} />
-      <div>teste</div>
+      <div>
+        {charactersResponse?.data?.results?.map((character) => (
+          <div key={character.id}>{character.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
