41368b970dc95eed2265360a317cf9dbe6b224c2
coltran3 <lcssantana03@gmail.com>
2024-11-17 19:32:45 -0300

    feat: add interceptor to axios client

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import crypto from "crypto";
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -6,3 +7,31 @@ export const apiClient = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const ts = new Date().getTime().toString();
+    const hash = crypto
+      .createHash("md5")
+      .update(
+        ts +
+          process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY +
+          process.env.NEXT_PUBLIC_MARVEL_PRIVATE_KEY
+      )
+      .digest("hex");
+
+    const configParams = config.params ? config.params : {};
+
+    config.params = {
+      ...configParams,
+      ts,
+      hash,
+      apikey: process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY,
+    };
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
