916ef6090e88070b458427f29ad7b6bba3b6932d
coltran3 <lcssantana03@gmail.com>
2024-11-17 20:42:24 -0300

    feat: add debounce to query and use character card to display character

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,26 +2,38 @@
 import { useEffect, useState } from "react";
 import { ApplicationCommand } from "./components/command";
 import { useCharacters } from "@/hooks/queries/use-characters";
+import { CharacterCard } from "./components/character-card";
 
 export default function Dashboard() {
   const [query, setQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, 300);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [query]);
 
   const { data: charactersResponse } = useCharacters({
     limit: 10,
     offset: 0,
-    nameStartsWith: query,
-  });
-
-  useEffect(() => {
-    console.log(charactersResponse);
+    nameStartsWith: debouncedQuery,
   });
 
   return (
-    <div className="flex flex-col w-full p-3 ">
+    <div className="flex flex-col w-full p-3 gap-3">
       <ApplicationCommand value={query} onValueChange={setQuery} />
-      <div>
+      <div className="self-center flex gap-2 flex-wrap mx-auto justify-center">
         {charactersResponse?.data?.results?.map((character) => (
-          <div key={character.id}>{character.name}</div>
+          <CharacterCard
+            key={character.id}
+            title={character.name}
+            imgSrc={`${character.thumbnail?.path}.${character.thumbnail?.extension}`}
+          />
         ))}
       </div>
     </div>
