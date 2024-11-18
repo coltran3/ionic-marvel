88831aa087e3d97d31a3bdc40f49ef089064ed60
coltran3 <lcssantana03@gmail.com>
2024-11-17 23:09:27 -0300

    feat: create character page

---
+++ src/app/dashboard/character/[characterId]/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { TypographyH2, TypographyLarge } from "@/components/ui/typography";
+import { useCharacter } from "@/hooks/queries/use-character";
+import { getImgSrc } from "@/lib/utils";
+
+interface CharacterPageProps {
+  params: { characterId: string };
+}
+
+export default function CharacterPage({
+  params: { characterId },
+}: CharacterPageProps) {
+  const { data: characterResponse, isLoading } = useCharacter({ characterId });
+
+  const character = characterResponse?.data?.results?.[0];
+  return (
+    <div className="p-3">
+      {isLoading ? (
+        <div>Carregando personagem...</div>
+      ) : (
+        <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-2">
+            <TypographyH2>{character?.name}</TypographyH2>
+            <TypographyLarge>{character?.description}</TypographyLarge>
+          </div>
+
+          <div className="p-1 border-gray-300 rounded-md border self-start">
+            <img
+              className="w-96 h-96"
+              src={getImgSrc({
+                path: character?.thumbnail?.path ?? "",
+                extension: character?.thumbnail?.extension ?? "",
+              })}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
