e480fa2714d7039e98b61c2fdfb1f2e36314440d
coltran3 <lcssantana03@gmail.com>
2024-11-17 22:20:06 -0300

    feat: add loading and pagination logic to characters page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,14 @@ import { useEffect, useState } from "react";
 import { ApplicationCommand } from "./components/command";
 import { useCharacters } from "@/hooks/queries/use-characters";
 import { CharacterCard } from "./components/character-card";
+import { ApplicationPagination } from "@/components/app-pagination";
+import Link from "next/link";
+
+const PAGE_LIMIT = 15;
 
 export default function Dashboard() {
   const [query, setQuery] = useState("");
+  const [offset, setOffset] = useState(0);
   const [debouncedQuery, setDebouncedQuery] = useState("");
 
   useEffect(() => {
@@ -18,24 +23,43 @@ export default function Dashboard() {
     };
   }, [query]);
 
-  const { data: charactersResponse } = useCharacters({
-    limit: 10,
-    offset: 0,
+  const { data: charactersResponse, isLoading } = useCharacters({
+    limit: PAGE_LIMIT,
+    offset: offset,
     nameStartsWith: debouncedQuery,
   });
+
+  function handleChangePage(nextPage: number) {
+    setOffset(nextPage * PAGE_LIMIT);
+  }
 
   return (
     <div className="flex flex-col w-full p-3 gap-3">
       <ApplicationCommand value={query} onValueChange={setQuery} />
-      <div className="self-center flex gap-2 flex-wrap mx-auto justify-center">
-        {charactersResponse?.data?.results?.map((character) => (
-          <CharacterCard
-            key={character.id}
-            title={character.name}
-            imgSrc={`${character.thumbnail?.path}.${character.thumbnail?.extension}`}
-          />
-        ))}
-      </div>
+      {isLoading ? (
+        <div className="mx-auto">Carregando personagens...</div>
+      ) : (
+        <div className="self-center flex gap-2 flex-wrap mx-auto justify-center">
+          {charactersResponse?.data?.results?.map((character) => (
+            <Link href={`/dashboard/character/${character.id}`}>
+              <CharacterCard
+                key={character.id}
+                title={character.name}
+                imgSrc={`${character.thumbnail?.path}.${character.thumbnail?.extension}`}
+              />
+            </Link>
+          ))}
+        </div>
+      )}
+      <ApplicationPagination
+        onChangePage={handleChangePage}
+        currentPage={offset / PAGE_LIMIT}
+        pagesAmount={
+          charactersResponse?.data?.total
+            ? Math.floor(charactersResponse.data.total / PAGE_LIMIT)
+            : 0
+        }
+      />
     </div>
   );
 }
