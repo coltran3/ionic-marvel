1d1114a4ecfffaa214ea78c3a578211f9f609a21
coltran3 <lcssantana03@gmail.com>
2024-11-17 23:35:26 -0300

    feat: add types related to event

---
+++ src/api/types/event.ts
@@ -0,0 +1,58 @@
+import { CharacterSummary } from "./character";
+import {
+  ComicList,
+  DataContainer,
+  Image,
+  Response,
+  SeriesList,
+  StoryList,
+  Url,
+} from "./common";
+
+export type EventResponse = Response<EventDataContainer>;
+
+export type EventDataContainer = DataContainer<Event>;
+
+export interface Event {
+  id?: number;
+  title?: string;
+  description?: string;
+  resourceURI?: string;
+  urls?: Url[];
+  modified?: Date;
+  start?: Date;
+  end?: Date;
+  thumbnail?: Image;
+  comics?: ComicList;
+  stories?: StoryList;
+  series?: SeriesList;
+  characters?: CharacterList;
+  creators?: CreatorList;
+  next?: EventSummary;
+  previous?: EventSummary;
+}
+
+export interface CharacterList {
+  available?: number;
+  returned?: number;
+  collectionURI?: string;
+  items?: CharacterSummary[];
+}
+
+export interface CreatorList {
+  available?: number;
+  returned?: number;
+  collectionURI?: string;
+  items?: CreatorSummary[];
+}
+
+export interface CreatorSummary {
+  resourceURI?: string;
+  name?: string;
+  role?: string;
+}
+
+export interface EventSummary {
+  resourceURI?: string;
+  name?: string;
+}
